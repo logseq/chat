8c859daf594632f575c70901130133cbb80f419a
Tienson Qin <tiensonqin@gmail.com>
2026-09-23 09:07:13 -0700

    Bump lui to 38096c4

--- apple/Package.swift
+++ apple/Package.swift
@@ -34,7 +34,7 @@ let package = Package(
         .library(name: "LogseqChatModel", type: .dynamic, targets: ["LogseqChatModel"]),
     ],
     dependencies: [
-        .package(url: "ssh://git@github.com/logseq/lui.git", revision: "81bcb3687a323127a8e894a5b4f1a3bf60474484"),
+        .package(url: "ssh://git@github.com/logseq/lui.git", revision: "38096c4fab853e462e60fc04d4daadd1f06aa09a"),
         .package(url: "https://github.com/gonzalezreal/swiftui-math", from: "0.1.0"),
         .package(url: "https://github.com/appstefan/highlightswift.git", from: "1.1.0")
     ],
