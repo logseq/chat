5351e469c797fd19d6da9465242cdd73ac8abebc
Tienson Qin <tiensonqin@gmail.com>
2026-09-23 12:40:17 +0000

    Bump pin-depends: lui, datascript-ocaml, ocaml-signal, mldoc, ocaml-fsrs, melange-transit

--- apple/Package.swift
+++ apple/Package.swift
@@ -34,7 +34,7 @@ let package = Package(
         .library(name: "LogseqChatModel", type: .dynamic, targets: ["LogseqChatModel"]),
     ],
     dependencies: [
-        .package(url: "ssh://git@github.com/logseq/lui.git", revision: "b1cb9e12659285ae78f973545d840444a439d9b3"),
+        .package(url: "ssh://git@github.com/logseq/lui.git", revision: "81bcb3687a323127a8e894a5b4f1a3bf60474484"),
         .package(url: "https://github.com/gonzalezreal/swiftui-math", from: "0.1.0"),
         .package(url: "https://github.com/appstefan/highlightswift.git", from: "1.1.0")
     ],
