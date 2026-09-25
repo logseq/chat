36bea880ebbe2dcc2ff7530f53a6357a3675d652
Tienson Qin <tiensonqin@gmail.com>
2026-09-25 17:19:27 +0000

    bump lui pin for autofocus edge + flutter semantics fixes

--- apple/Package.swift
+++ apple/Package.swift
@@ -34,7 +34,7 @@ let package = Package(
         .library(name: "LogseqChatModel", type: .dynamic, targets: ["LogseqChatModel"]),
     ],
     dependencies: [
-        .package(url: "ssh://git@github.com/logseq/lui.git", revision: "4807689"),
+        .package(url: "ssh://git@github.com/logseq/lui.git", revision: "ed83fdb"),
         .package(url: "https://github.com/gonzalezreal/swiftui-math", from: "0.1.0"),
         .package(url: "https://github.com/appstefan/highlightswift.git", from: "1.1.0")
     ],
