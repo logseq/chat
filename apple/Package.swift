4d247c98e75a9ec5a8a1f70fec133984ceccf2ef
Tienson Qin <tiensonqin@gmail.com>
2026-09-26 11:28:14 -0700

    pin lui to merged main commit 55166025

--- apple/Package.swift
+++ apple/Package.swift
@@ -34,7 +34,7 @@ let package = Package(
         .library(name: "LogseqChatModel", type: .dynamic, targets: ["LogseqChatModel"]),
     ],
     dependencies: [
-        .package(url: "ssh://git@github.com/logseq/lui.git", revision: "b4aca63c"),
+        .package(url: "ssh://git@github.com/logseq/lui.git", revision: "55166025"),
         .package(url: "https://github.com/gonzalezreal/swiftui-math", from: "0.1.0"),
         .package(url: "https://github.com/appstefan/highlightswift.git", from: "1.1.0")
     ],
