d51455216e659beda2df5606447369e9ed9628e5
Tienson Qin <tiensonqin@gmail.com>
2026-09-26 11:16:36 -0700

    pin lui to b4aca63c (scroll capsule fix)

--- apple/Package.swift
+++ apple/Package.swift
@@ -34,7 +34,7 @@ let package = Package(
         .library(name: "LogseqChatModel", type: .dynamic, targets: ["LogseqChatModel"]),
     ],
     dependencies: [
-        .package(url: "ssh://git@github.com/logseq/lui.git", revision: "912829b9"),
+        .package(url: "ssh://git@github.com/logseq/lui.git", revision: "b4aca63c"),
         .package(url: "https://github.com/gonzalezreal/swiftui-math", from: "0.1.0"),
         .package(url: "https://github.com/appstefan/highlightswift.git", from: "1.1.0")
     ],
